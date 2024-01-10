50e163d1f0b95a9280a733acd05a70e6a1b192d9
soham <sohamsalunke10@gmail.com>
2024-01-10 15:25:48 -0500

    fix again

--- src/app/auth/components/AuthComponent.tsx
+++ src/app/auth/components/AuthComponent.tsx
@@ -9,7 +9,7 @@ export default function AuthComponent() {
 		supabase.auth.signInWithOAuth({
 			provider: "github",
 			options: {
-				redirectTo: 'https://easyres.vercel.app/' + "/auth/callback",
+				redirectTo: 'https://easyres.vercel.app' + "/auth/callback",
 			},
 		});
 	};
@@ -18,7 +18,7 @@ export default function AuthComponent() {
     supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: 'https://easyres.vercel.app/' + "/auth/callback" ,
+            redirectTo: 'https://easyres.vercel.app' + "/auth/callback" ,
         },
     });
   };
